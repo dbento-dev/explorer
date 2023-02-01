e45209fdb512483c35456b5674fae3b20ddca60c
David Bento <dbento.dev@gmail.com>
2023-02-01 18:50:46 -0300

    create Profile page

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -8,6 +8,7 @@ import GlobalStyles from './styles/global'
 import { Home } from './pages/Home'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,7 +17,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       {/* <Details /> */}
       {/* <Home /> */}
       {/* <SignIn /> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
