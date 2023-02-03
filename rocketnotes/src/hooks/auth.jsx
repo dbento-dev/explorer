89e2b5d8e37ebb7e53874d94c5e206b24ed16dc5
David Bento <dbento.dev@gmail.com>
2023-02-03 17:49:12 -0300

    add token and user infos to local storage

---
+++ rocketnotes/src/hooks/auth.jsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState, useEffect } from 'react'
+import { api } from '../services/api'
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }) {
+  const [userData, setUserData] = useState({})
+
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post('/sessions', { email, password })
+      const { user, token } = response.data
+
+      localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
+      localStorage.setItem('@rocketnotes:token', token)
+
+      api.defaults.headers.Authorization = `Bearer ${token}`
+
+      setUserData({ user, token })
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('Não foi possível fazer o login, tente novamente mais tarde.')
+      }
+    }
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@rocketnotes:token')
+    const user = localStorage.getItem('@rocketnotes:user')
+
+    if (token && user) {
+      api.defaults.headers.Authorization = `Bearer ${token}`
+      setUserData({ token, user: JSON.parse(user) })
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ signIn, user: userData.user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext)
+  return context
+}
+
+export { AuthProvider, useAuth }
--- rocketnotes/src/pages/SignIn/index.jsx
+++ rocketnotes/src/pages/SignIn/index.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
+
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { Container, Form, Background } from './styles'
-
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-
 import { Link } from 'react-router-dom'
 
+import { useAuth } from '../../hooks/auth'
+
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn, user } = useAuth()
+
+  const handleSignIn = () => {
+    if (!email || !password) {
+      alert('Preencha todos os campos')
+      return false
+    } else {
+      signIn({ email, password })
+    }
+  }
+
   return (
     <Container>
       <Form>
@@ -14,10 +30,20 @@ export function SignIn() {
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
